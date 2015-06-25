a950523e2165c9d3cc907d29faa9548404b63619
Nop Jiarathanakul <nop@iamnop.com>
2015-06-25 14:36:04 -0700

    remove unused things and other misc setups

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,31 +8,32 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 */
 
 (function(document) {
-  'use strict';
+  "use strict";
 
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  var app = document.querySelector("#app");
 
   app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
+    document.querySelector("#caching-complete").show();
   };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+  app.addEventListener("dom-change", function() {
+    console.log("Polymer App Ready");
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener("WebComponentsReady", function() {
     // imports are loaded and elements have been registered
+    console.log("Web Components Ready");
   });
 
   // Close drawer after menu item is selected if drawerPanel is narrow
   app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    var drawerPanel = document.querySelector("#paperDrawerPanel");
     if (drawerPanel.narrow) {
       drawerPanel.closeDrawer();
     }
