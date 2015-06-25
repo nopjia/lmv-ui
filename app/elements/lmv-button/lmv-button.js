447fdf6499f21dc0e5888dd98f158c36776156eb
Nop Jiarathanakul <nop@iamnop.com>
2015-06-25 14:56:45 -0700

    try building sample lmv-button with separate js file

---
+++ app/elements/lmv-button/lmv-button.js
@@ -0,0 +1,13 @@
+(function() {
+  Polymer({
+    is: "lmv-button",
+
+    properties: {
+      foo: {
+        type: String,
+        value: "bar",
+        notify: true
+      }
+    }
+  });
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task("copy", function () {
     "bower_components/**/*"
   ]).pipe(gulp.dest("dist/bower_components"));
 
-  var elements = gulp.src(["app/elements/**/*.html"])
+  var elements = gulp.src(["app/elements/**/*.{html,js}"])
     .pipe(gulp.dest("dist/elements"));
 
   var swBootstrap = gulp.src(["bower_components/platinum-sw/bootstrap/*.js"])
