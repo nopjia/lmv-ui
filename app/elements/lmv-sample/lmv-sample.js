6972aac84f003df1004d9c399438791b3ea66477
Nop Jiarathanakul <nop@iamnop.com>
2015-06-26 11:15:58 -0700

    play with polymer features

--- app/elements/lmv-sample/lmv-sample.js
+++ app/elements/lmv-sample/lmv-sample.js
@@ -3,14 +3,90 @@
     is: "lmv-sample",
 
     properties: {
-      hello: {
+      greeting: {
         type: String,
-        value: "LMV Rocks"
+        value: "Hello",
+        reflectToAttribute: true,
+        observer: "greetingChanged",
+      },
+      first: {
+        type: String,
+        value: "Nancy"
+      },
+      last: {
+        type: String,
+        value: "Lee"
+      },
+      fullname: {
+        type: String,
+        computed: "computeFullname(first, last)"
+      },
+    },
+    observers: [
+      "somethingChanged(greeting, first, last)"
+    ],
+
+    listeners: {
+      tap: "handleTap",
+      track: "handleTrack"
+    },
+
+
+    // ready
+
+    ready: function() {
+      console.log("lmv-sample ready");
+    },
+
+
+    // computed properties
+
+    computeFullname: function(first, last) {
+      return first + " " + last;
+    },
+
+
+    // observers
+
+    greetingChanged: function(newVal, oldVal) {
+      console.log("greeting changed '" + oldVal + "' to '" + newVal + "'");
+    },
+
+    somethingChanged: function(greeting, first, last) {
+      console.log("something changed: " + greeting + " " + first + " " + last);
+    },
+
+
+    // event listeners
+
+    handleTap: function(e) {
+      console.log("Tracking "+ "(" + e.detail.x + "," + e.detail.y + ")");
+    },
+
+    handleTrack: function(e) {
+      switch(e.detail.state) {
+        case "start":
+          console.log("Tracking started");
+          break;
+        case "track":
+          console.log("Tracking "+
+            "(" + e.detail.x + "," + e.detail.y + ")\t" +
+            "(" + e.detail.dx + "," + e.detail.dy + ")\t" +
+            "(" + e.detail.ddx + "," + e.detail.ddy + ")\t"
+          );
+          break;
+        case "end":
+          console.log("Tracking ended");
+          break;
       }
     },
 
-    onInput: function() {
-      console.log("onInput" + this.hello);
+
+    // functions
+
+    onInput: function(e) {
+      console.log("onInput " + e.target.value);
     },
+
   });
 })();
